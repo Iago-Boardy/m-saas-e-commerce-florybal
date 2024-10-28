31757011fcc58af2551432dcc543f6dbf2832b28
Iago Boardman Padilha <iagogol2006@gmail.com>
2024-10-28 17:03:59 -0300

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,29 @@
-import type { Metadata } from "next";
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/navbar";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-export const metadata: Metadata = {
-  title: "Ecommerce Model",
-  description: "Created by Iago Boardy",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  // Renderiza a Navbar apenas se não estiver na rota /admin
+  const showNavbar = !pathname.startsWith("/admin");
+
   return (
-    <html lang="en">  
-      <body className={cn("bg-background min-h-screen font-sans antialiased", inter.variable)}><Navbar/>{children}</body>
+    <html lang="en">
+      <body className={cn("bg-background min-h-screen font-sans antialiased", inter.variable)}>
+        {showNavbar && <Navbar />}
+        {children}
+      </body>
     </html>
   );
 }
