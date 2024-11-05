dd0bc6f73be77eb7ca7f86390c56eb089b609b9f
Iago Boardy <iagogol2006@gmail.com>
2024-11-05 18:48:23 -0300

    Commit the middleware

--- routes.ts
+++ routes.ts
@@ -1,12 +1,15 @@
+//Rotas publicas
 export const publicRoutes = [
-  "/"
+  "/",
 ]
-
+//Rotas auth padrao
 export const authRoutes = [
   "/auth/login",
   "/auth/register"
 ]
 
+//Rota api auth padrao
 export const apiAuthPrefix = "/api/auth"
 
+//Rota de redirect
 export const DEFAULT_LOGIN_REDIRECT = "/products"
