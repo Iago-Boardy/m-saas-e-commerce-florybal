49faee96c21a52e1288ee1c64fbdcc0bc906659f
Iago Boardy <iagogol2006@gmail.com>
2024-11-07 18:39:50 -0300

    Admin Dashboard done

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,62 @@
-"use client"
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { db } from "@/lib/db"
+import { formatCurrency, formatNumber } from "@/lib/formatters"
 import { CalendarDays, CreditCard, DollarSign, Users } from "lucide-react"
 
+async function getSalesData() {
+    const data = await db.order.aggregate({
+        _sum: {pricePaidInCents: true},
+        _count: true
+    })
 
+    return {
+        amount: data._sum.pricePaidInCents || 0,
+        numberOfSales: data._count
+    }
+}
 
-export default function AdminHome() {
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    await db.order.aggregate({
+      _sum: { pricePaidInCents: true}
+    })
+  ])
+
+  return {
+    userCount,
+    avarageValuePerUser: userCount === 0 ? 0: (orderData._sum.pricePaidInCents || 0) / userCount / 100,
+  }
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all ([
+    db.product.count({where: { isAvaliableForPurchase: true}}),
+    db.product.count({where: { isAvaliableForPurchase: false}}),
+  ])
+
+  return {
+    activeCount,
+    inactiveCount
+  }
+}
+
+export default async function AdminHome() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData() 
+  ])
+
   return (
     <div className="flex flex-col w-full">
       <main className="flex-1 lg:px-8 px-4 py-6 space-y-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <DashboardCard title="Sales" subtitle="R$200" body="yea, thats it" icon={<DollarSign className="h-4 w-4 text-muted-foreground" />} />
-          <DashboardCard title="Users" subtitle="1,200" body="Total users" icon={<Users className="h-4 w-4 text-muted-foreground" />} />
-          <DashboardCard title="Events" subtitle="150" body="Total events" icon={<CalendarDays className="h-4 w-4 text-muted-foreground" />} />
+        <DashboardCard title="Sales" subtitle={`${formatNumber(salesData.numberOfSales)} Pedidos`} body={formatCurrency(salesData.amount)} icon={<DollarSign className="h-4 w-4 text-muted-foreground" />} />
+        <DashboardCard title="Clientes" subtitle={`${formatCurrency(userData.avarageValuePerUser)} Valor médio`} body={formatNumber(userData.userCount)} icon={<Users className="h-4 w-4 text-muted-foreground" />} />
+        <DashboardCard title="Produtos" subtitle={`${formatNumber(productData.inactiveCount)} Valor médio`} body={formatNumber(productData.activeCount)} icon={<CreditCard className="h-4 w-4 text-muted-foreground" />} />
+
         </div>
       </main>
     </div>
